674c0fa7b8b8f125b78881cf98be8be8aab77582
MSR333UA <matviivserhii@gmail.com>
2023-10-25 11:42:00 +0100

    add pl

---
+++ public/image/plugins/akeebabackupwp/helpers/private/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+global $table_prefix;
+$table_prefix = 'wp_';
+define('DB_NAME', 'cl38-purplebr');
+define('DB_USER', 'cl38-purplebr');
+define('DB_PASSWORD', '/ounders@6854@');
+define('DB_HOST', 'localhost');
