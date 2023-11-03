16e4b2dd918575e8e143d220dd522a949b3e2b28
MSR333UA <matviivserhii@gmail.com>
2023-11-03 13:59:30 +0000

    clean

--- public/image/themes/bakery/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Type and hit enter', 'bakery'); ?>" name="s" value="<?php echo get_search_query() ?>"/>
-	<input type="submit" class="search-submit hide" value="<?php echo esc_attr_x( 'Search', 'bakery'); ?>" />
-</form>
