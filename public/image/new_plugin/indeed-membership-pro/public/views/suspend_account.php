65404e3145f2b1905d0d3de10cd499ba160122c4
MSR333UA <matviivserhii@gmail.com>
2023-11-10 15:10:49 +0000

    add text

--- public/image/new_plugin/indeed-membership-pro/public/views/suspend_account.php
+++
@@ -1,4 +0,0 @@
-<form action="" method="post" class="ihc-suspend-account-wrapper"> 
-	<input type="submit" value="<?php _e('Suspend My Account', 'ihc');?>" />
-	<input type="hidden" name="ihcaction" value="suspend" /> 
-</form>
